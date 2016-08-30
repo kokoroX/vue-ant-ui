ada3878563c6304366137ec898b246473ec1851a
kokoro <8462304@qq.com>
2016-08-31 00:34:56 +0800

    创建文档

--- example/affix.vue
+++
@@ -1,8 +0,0 @@
-<template lang="html">
-  <div style="height: 2000px;">
-    <div style="height: 900px;"></div>
-    <ant-affix :offset-bottom="70">
-      <ant-button type="primary">固定在顶部</ant-button>
-    </ant-affix>
-  </div>
-</template>
